ebb01eb3f8f864c149245ced30cfa2978e3f26a2
alessandrogama <alessandroolavogama@gmail.com>
2026-08-11 21:15:54 -0300

    feat: Change packge main

--- src/main/java/br/com/alessandro/countryapi/LabPadroesProjetoSpringApplication.java
+++ src/main/java/br/com/alessandro/countryapi/LabPadroesProjetoSpringApplication.java
@@ -1,4 +1,4 @@
-package one.digitalinnovation.gof;
+package br.com.alessandro.countryapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
